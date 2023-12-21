b1aba86c8beeca6b76952383900592176b0d1286
Michael A. Merchant <83989128+RemoteSenseiMichael@users.noreply.github.com>
2023-12-21 12:01:37 -0800

    Update Python_Code_XAI_Shapley_Classification.py

--- Python_Code_XAI_Shapley_Classification.py
+++ Python_Code_XAI_Shapley_Classification.py
@@ -1,197 +1,73 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import pandas as pd
-
-
-# In[2]:
-
-
 from sklearn.model_selection import train_test_split, GridSearchCV
-
-
-# In[3]:
-
-
 from sklearn.ensemble import RandomForestRegressor
-
-
-# In[4]:
-
-
-from sklearn.ensemble import RandomForestClassifier
-
-
-# In[5]:
-
-
-from sklearn.metrics import mean_squared_error, make_scorer, r2_score, accuracy_score
-
-
-# In[6]:
-
-
+from sklearn.metrics import mean_squared_error, make_scorer, r2_score
 from sklearn.model_selection import KFold
-
-
-# In[7]:
-
-
 import matplotlib.pyplot as plt
-
-
-# In[8]:
-
-
 import shap
 
+# Import your data as a CSV file containing your samples.
+# The first column should be the target variable. The remaining columns should be the predictor variables and their values.
+data = pd.read_csv('E:/Samples.csv')
 
-# In[9]:
-
-
-# Import your data containing samples:
-data = pd.read_csv('E:/013_Projects/016_ABMI/016_Prototypes/ABMI_Pilots_ML/Boreal_2/temp2/Boreal_2_Samples_NoALOS.csv')
-
-
-# In[10]:
-
-
-# Split data into input features and target variable
+# Split data into input features and target variable which in this case is 'class'.
 X = data.drop(columns=['class'])  # Features (input covariates)
 y = data['class']  # Target variable
 
-
-# In[11]:
-
-
-# GEE Parameters
+# Here is a comparision list between ee.Classifier.smileRandomForest hyperparameter names and  sklearn Random Forest for reference:
 # n_estimators = numberOfTrees
 # max_depth = maxNodes
 # min_samples_split = variablesPerSplit
 # min_samples_leaf = minLeafPopulation
 
-
-# In[13]:
-
-
+# Set the values for each hyperparameter you would like to optimize (tune) for your Random Forest model.
 param_grid = {
-    'n_estimators': [100, 300, 500],
-    'max_depth': [None, 10, 20, 30],
-    'min_samples_split': [2, 5, 10, 15],
-    'min_samples_leaf': [1, 2, 4, 8]
+    'n_estimators': [100, 200, 300],
+    'max_depth': [None, 10, 20],
+    'min_samples_split': [2, 5, 10],
+    'min_samples_leaf': [1, 2, 4]
 }
 
+# Create your Random Forest model.
+rf_model = RandomForestRegressor(random_state=42)
 
-# In[14]:
-
-
-rf_model = RandomForestClassifier()
-
-
-# In[15]:
-
-
-# Step 6: Use GridSearchCV for hyperparameter tuning and 5-fold cross-validation
+# Use GridSearchCV for hyperparameter tuning and 5-fold cross-validation. Choose best parameters based on R².
 kf = KFold(n_splits=5, shuffle=True, random_state=42)
 scoring = make_scorer(accuracy_score)
-#grid_search = GridSearchCV(estimator=rf_model, param_grid=param_grid, scoring='neg_mean_squared_error', cv=kf, n_jobs=-1)
 grid_search = GridSearchCV(estimator=rf_model, param_grid=param_grid, scoring=scoring, cv=kf, n_jobs=-1)
 grid_search.fit(X, y)
 
-
-# In[16]:
-
-
-# Step 7: Extract R² values for each hyperparameter combination
+# Extract accuracy values for each hyperparameter combination.
 results = pd.DataFrame(grid_search.cv_results_)
 
+# Create accuracy values.
+acc_values = results[['param_n_estimators', 'param_max_depth', 'param_min_samples_split', 'param_min_samples_leaf', 'mean_test_score']]
 
-# In[17]:
+# Export R² values to a CSV file.
+acc_values.to_csv('E:/RF_Tuning_Results.csv', index=False)
 
-
-accuracy_values = results[['param_n_estimators', 'param_max_depth', 'param_min_samples_split', 'param_min_samples_leaf', 'mean_test_score']]
-
-
-# In[18]:
-
-
-# Step 8: Export R² values to a CSV file
-accuracy_values.to_csv('E:/013_Projects/016_ABMI/016_Prototypes/ABMI_Pilots_ML/Boreal_2/temp2/Boreal_2_CV.csv', index=False)
-
-
-# In[19]:
-
-
+# Build optimized Random Forest model based on best accuracy.
 best_rf_model = grid_search.best_estimator_
 best_params = grid_search.best_params_
-best_accuracy = grid_search.best_score_
+best_acc = grid_search.best_score_
 
-
-# In[20]:
-
-
+# Print the optimized hyperparametes and R² achieved on cross-validation.
 print("Best Hyperparameters:", best_params)
-print("Best R-squared (R²):", best_accuracy)
+print("Best accuracy:", best_acc)
 
-
-# In[21]:
-
-
-import shap
-
-
-# In[22]:
-
-
-# custom_colors = ("blue", "red", "yellow", "orange", "black")  # Set your desired colors here
-
-
-# In[23]:
-
-
+# Use the Shapley Explainer function to analyze your optimzed Random Forest model.
 explainer = shap.Explainer(best_rf_model, X, check_additivity=False)
 
-
-# In[ ]:
-
-
-
-
-
-# In[24]:
-
-
+# See (view) the Shap summary plot:
 shap_values = explainer.shap_values(X, check_additivity=False)
 
+# Link for choosing bar colors: https://matplotlib.org/stable/users/explain/colors/colormaps.html
 
-# In[ ]:
-
-
-# Choosing colors: https://matplotlib.org/stable/users/explain/colors/colormaps.html
-
-
-# In[25]:
-
-
-# See the summary plot:
+# See (view) the summary plot:
 shap.summary_plot(shap_values, X, max_display=20, plot_type="bar", color=plt.get_cmap("tab20c"))
-
-
-# In[27]:
-
 
 # Export the summary plot.
 plt.figure()
 shap.summary_plot(shap_values, X, max_display=20, plot_type="bar", color=plt.get_cmap("tab20c"), show=False)
-plt.savefig('E:/013_Projects/016_ABMI/016_Prototypes/ABMI_Pilots_ML/Boreal_2/temp2/Boreal_2_SHAP.jpg', dpi=600)
+plt.savefig('E:/SHAP.jpg', dpi=600)
 plt.close()
-
-
-# In[ ]:
-
-
-
-
